9c6d829c516263bbe45e54ea42c04c7b0a282f9c
WhiteVax <WhiteVax@gmail.com>
2022-06-05 17:36:09 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (var reader = new BufferedReader(new FileReader(file))) {
+            list = reader.lines().filter(e -> e.contains("404 1"))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        var logFilter = new LogFilter();
+        List<String> log = logFilter.filter("C:\\projects"
+                + "\\job4j_design\\src\\main\\java\\ru\\job4j\\io\\log.txt");
+        log.forEach(System.out ::println);
+    }
+}

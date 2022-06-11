d16f7ec4f9eb3a714cc00443cdece3064093d82e
WhiteVax <WhiteVax@gmail.com>
2022-06-11 20:29:36 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,47 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Config {
+
+    private final String path;
+    private final HashMap<String, String> values = new HashMap<String, String>();
+
+    public Config(final String path) {
+        this.path = path;
+    }
+
+    public void load() {
+        try (var reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines()
+                    .filter(e -> e.contains("="))
+                    .forEach(e -> {
+                        String[] s = e.split("=");
+                        if (s.length != 2 || s[0].isEmpty() || s[1].isEmpty()) {
+                            throw new IllegalArgumentException();
+                        }
+                        values.put(s[0], s[1]);
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String value(String key) {
+        return values.get(key);
+    }
+
+    @Override
+    public String toString() {
+        var joiner = new StringJoiner(System.lineSeparator());
+        try (var reader = new BufferedReader(new FileReader(this.path))) {
+            reader.lines().forEach(joiner::add);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return joiner.toString();
+    }
+}
---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithCommentAndWithHoldLine() {
+        var path = "./data/app.properties";
+        var config = new Config(path);
+        config.load();
+        assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        assertThat(config.value("hib"), is(Matchers.nullValue()));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void whenPaisWithViolationTemple() {
+        var path = "./data/with_violation_of_temple.properties";
+        var config = new Config(path);
+        config.load();
+    }
+}

353c04868afa0975ea69e3a64ae9e44e63e5b606
WhiteVax <WhiteVax@gmail.com>
2022-06-26 22:56:32 +0300

    4.0. File [#252491 #316242]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -7,14 +7,14 @@ public class Dir {
     public static void main(String[] args) {
         var file = new File("C:\\projects");
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Not exists %s" + file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not exists %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
-            throw new IllegalArgumentException(String.format("Not directory %s" + file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
         }
-        System.out.println(file.getName() + " : " + file.getTotalSpace());
+        System.out.printf("size : %d%n", file.getTotalSpace());
         for (var subfile : Objects.requireNonNull(file.listFiles())) {
-            System.out.println(subfile.getName() + " : " + subfile.length());
+            System.out.printf("%s : %d%n", subfile.getName(), subfile.length());
         }
     }
 }

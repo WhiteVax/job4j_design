7a7a97acd612802b01404325567ceb006e02c826
WhiteVax <WhiteVax@gmail.com>
2022-07-05 18:33:29 +0300

    4.2. Поиск дубликатов [#315066 #318467]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -6,18 +6,30 @@ import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
 
-    private HashMap<FileProperty, String> files = new HashMap<>();
+    private final Map<FileProperty, String> files = new HashMap<>();
+    private final Set<FileProperty> duplicates = new HashSet<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         var fileProperty = new FileProperty(files.size(), file.getFileName().toAbsolutePath().toString());
         if (files.containsKey(fileProperty)) {
-            System.out.println(fileProperty.getName());
+            duplicates.add(fileProperty);
         }
-        files.put(fileProperty, fileProperty.getName());
+        files.putIfAbsent(fileProperty, fileProperty.getName());
         return super.visitFile(file, attrs);
+    }
+
+    public void  printDuplicates() {
+        if (duplicates.isEmpty()) {
+            System.out.println("Not found duplicates.");
+        } else {
+            duplicates.forEach(e -> System.out.println(e.getName()));
+        }
     }
 }

7d122e536badbb651718b6d67007912aafcfc6ee
WhiteVax <WhiteVax@gmail.com>
2022-09-06 18:11:46 +0300

    2. Найти утечку памяти. [#504882]

---
+++ src/main/java/ru/job4j/gc/leak/Comment.java
@@ -0,0 +1,21 @@
+package ru.job4j.gc.leak;
+
+public class Comment {
+
+    private String text;
+
+    private User user;
+
+    public Comment(String text, User user) {
+        this.text = text;
+        this.user = user;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
---
+++ src/main/java/ru/job4j/gc/leak/CommentGenerator.java
@@ -0,0 +1,55 @@
+package ru.job4j.gc.leak;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CommentGenerator implements Generate {
+
+    public static final String PATH_PHRASES = "src/main/java/ru/job4j/gc/leak/files/phrases.txt";
+
+    public static final String SEPARATOR = System.lineSeparator();
+
+    private static final List<Comment> COMMENTS = new ArrayList<>();
+
+    public static final int COUNT = 50;
+
+    private List<String> phrases;
+
+    private final UserGenerator userGenerator;
+
+    private final Random random;
+
+    public CommentGenerator(Random random, UserGenerator userGenerator) {
+        this.userGenerator = userGenerator;
+        this.random = random;
+        read();
+    }
+
+    private void read() {
+        try {
+            phrases = read(PATH_PHRASES);
+        } catch (IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static List<Comment> getComments() {
+        return COMMENTS;
+    }
+
+    @Override
+    public void generate() {
+        COMMENTS.clear();
+        List<Integer> ints = new ArrayList<>();
+        random.ints(0, phrases.size())
+                .distinct().limit(3).forEach(ints::add);
+        for (int i = 0; i < COUNT; i++) {
+            String comment = String.format("%s%s%s%s%s", phrases.get(ints.get(0)), SEPARATOR,
+                     phrases.get(ints.get(1)), SEPARATOR, phrases.get(ints.get(2)));
+            COMMENTS.add(new Comment(comment,
+                    userGenerator.randomUser()));
+        }
+    }
+}

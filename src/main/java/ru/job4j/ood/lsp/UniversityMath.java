28c5a414b7930f81bcef2fddb64862e8ad783870
WhiteVax <WhiteVax@gmail.com>
2022-09-19 15:53:30 +0300

    0. Принцип подстановки Лисков. [#4915 #344134]

---
+++ src/main/java/ru/job4j/ood/lsp/UniversityMath.java
@@ -0,0 +1,15 @@
+package ru.job4j.ood.lsp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UniversityMath extends University {
+    List<Student> list = new ArrayList<>();
+
+    @Override
+    public void addStudent(Student student) {
+        if (student.getAvg() > 11.0) {
+            students.add(student);
+        }
+    }
+}

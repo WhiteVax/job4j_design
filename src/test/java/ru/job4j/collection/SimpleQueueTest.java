2919b5653e57c32383214fb04e5a472f1423563e
white vax <whitevax@gmail.com>
2025-02-11 20:13:56 +0200

    Updating SimpleQueueTest.

--- src/test/java/ru/job4j/collection/SimpleQueueTest.java
+++ src/test/java/ru/job4j/collection/SimpleQueueTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.NoSuchElementException;
 
-class SimpleQueueTest {
+public class SimpleQueueTest {
     private SimpleQueue<Integer> queue;
 
     @BeforeEach

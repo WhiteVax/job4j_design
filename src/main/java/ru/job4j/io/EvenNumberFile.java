773e6f025a23a39e7a94794490772f8a0582939e
white vax <whitevax@gmail.com>
2025-04-22 22:55:48 +0300

    Refactoring EvenNumberFile.

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -2,15 +2,13 @@ package ru.job4j.io;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class EvenNumberFile {
     public static void main(String[] args) {
-        try (FileInputStream in = new FileInputStream("C:\\projects\\"
-                + "job4j_design\\src\\main\\java\\ru\\job4j\\io\\even.txt")) {
-            Scanner out = new Scanner(in);
-            while (out.hasNext()) {
-                System.out.println(out.nextInt() % 2 == 0);
+        try (FileInputStream in = new FileInputStream("src\\main\\java\\ru\\job4j\\io\\even.txt")) {
+            int read;
+            while ((read = in.read()) != -1) {
+                System.out.println(read % 2 == 0);
             }
         } catch (IOException e) {
             e.printStackTrace();

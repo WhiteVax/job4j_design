17ce1d1f73bc39759ede05462b96d1217c30524e
WhiteVax <whitevax@gmail.com>
2026-03-12 21:29:52 +0200

    Сортировки [#505155]

---
+++ src/main/java/ru/job4j/algo/IntervalMerge.java
@@ -0,0 +1,32 @@
+package ru.job4j.algo;
+
+import java.util.ArrayList;
+
+public class IntervalMerge {
+    public int[][] merge(int[][] intervals) {
+        shortArray(intervals);
+        int[] current = intervals[0];
+        ArrayList<int[]> result = new ArrayList<>();
+        result.add(current);
+        for (int i = 1; i < intervals.length; i++) {
+            int[] next = intervals[i];
+            if (next[0] <= current[1]) {
+                current[1] = Math.max(current[1], next[1]);
+            } else {
+                current = next;
+                result.add(current);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+
+    private void shortArray(int[][] intervals) {
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1][1] > intervals[i][1]) {
+                int [] temp = intervals[i - 1];
+                intervals[i - 1] = intervals[i];
+                intervals[i] = temp;
+            }
+        }
+    }
+}
